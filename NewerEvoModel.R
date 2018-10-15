aba23c24d197076c974931f05e48a94ec163a8ad
mcastronova <mac0079@auburn.edu>
2018-10-15 14:07:59 -0500

    Update NewerEvoModel.R

--- NewerEvoModel.R
+++ NewerEvoModel.R
@@ -29,39 +29,24 @@ REL_FITNESS <- function(X, Bf = 3, As = 5, Af = 2)
 # N is number of individuals per generations
 # MaxYear is number of years
 # h2 is heritability (matrix)
-# Vp (matrix)
 # mu (2 number vector)
 # P is the mutation rate and should be between .01 and .2
 # Ag is intercept of growth curve
 # Bg is slope of growth curve
-SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nrow = 2, ncol = 2, byrow = T), mu = c(10, 0.5), P = matrix(c(20, 0, 0, 0.1), byrow = T, nrow = 2, ncol = 2), Ag = 5, Bg = -0.5)
+SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nrow = 2, ncol = 2, byrow = T), mu = c(10, 0.5), P = matrix(c(20, 0, 0, 0.1), byrow = T, nrow = 2, ncol = 2), Ag = 5, Bg = -0.5, mut=0.1)
 {
-  
-  ################## Function Mutation ##################
-  # Apply mutation by randomly selecting N.mutations
-  # We used reflected gamma distribution from Keightley and Hill (1987), shape paramter specified by them
-  # rate value gives mutational variance of .0001*Ve
-  # choosing number of mutations - pois distribution with mean of .1 haploid genome; if assume 1000 loci, pois mean .005, they have about 5*`0^(-6) mutation rate per locus
-  # if choose between .005 and .1`assumes we have between 1000 and 20000 loci 
-  # X is 
-  MUTATION <- function(X, Iyear = 1)
-  {
-    Individuals[X,"Mutation",Iyear] <<- Individuals[X,"Mutation",Iyear] + sample(c(-1,1), 1) *rgamma(1, shape = 0.5, rate = 10)
-  } # End of mutation function
-  
-  
   ################# Main program #################
   set.seed(100)                  # Initialize random number generator
   G			<- diag(sqrt(diag(h2)*diag(P)))      # Calculate Additive genetic variance
-  browser()
+  #browser()
   diag(h2) <- 1
   G <- G %*% h2 %*% G
   E <- P-G   #environmental variances and covariances
   ROUNDING <- 1e-8    #to make sample work
   
   #set up array
-  Individuals <- array(0,dim = c(N,13,MaxYear)) #All individuals saved in matrix
-  colnames(Individuals) <- c("ID", "BirthPhen", "GrowPhen", "BirthGen", "GrowGen", "BirthEnv", "GrowEnv", "Sex", "Fitness", "Mutation", "Growth", "Alive", "Age")
+  Individuals <- array(0,dim = c(N,14,MaxYear)) #All individuals saved in matrix
+  colnames(Individuals) <- c("ID", "BirthPhen", "GrowPhen", "BirthGen", "GrowGen", "BirthEnv", "GrowEnv", "Sex", "Fitness", "BirthMut", "GrowMut", "Growth", "Alive", "Age")
   Individuals[,"ID",1] <- c(1:(N)) #add unique IDs
   Individuals[,"Sex",1] <- sample(c(0,1), N, replace = T) #set all sexes
   
@@ -77,8 +62,9 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
   Individuals[,c("BirthEnv","GrowEnv"),1] <- grfx(N, E, warn = F)  # Environmental values for intercept and slope
   
   # Phenotype values
-  # Phenotypic values (mean(average of parents) + genetic intercept + environmental intercept + genetic slope + environmental slope)*year
+  # Phenotypic values (mean(average of parents) + genetic intercept + environmental intercept + genetic slope + environmental slope)*age
   #population mean birth size and population mean growth rate
+  Individuals[,"BirthPhen",1]
   Individuals[,"GrowPhen",1] 	 <- (Individuals[,"BirthPhen",1] + Individuals[,"BirthGen",1] + Individuals[,"GrowGen",1] + Individuals[,"BirthEnv",1] + Individuals[,"GrowEnv",1])*Individuals[,"Age",1]
   
   # Winter Survival
@@ -89,13 +75,19 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
   Individuals[Individuals[,"Sex",1] == TRUE,"Fitness",1] <- REL_FITNESS(Individuals[Individuals[,"Sex",1] == TRUE,"GrowPhen",1])
   
   # Mutation values
+  # We used reflected gamma distribution from Keightley and Hill (1987), shape paramter specified by them
+  # rate value gives mutational variance of .0001*Ve
+  # choosing number of mutations - pois distribution with mean of .1 haploid genome; if assume 1000 loci, pois mean .005, they have about 5*`0^(-6) mutation rate per locus
+  # if choose between .005 and .1`assumes we have between 1000 and 20000 loci
   # Number of mutations using a Poisson distribution
   # scale parameter .01 and .2 gives us the mean mutations per diploid genome assuming between 1000 and 20000 loci per site mutation rate 5*10^(-6)
   # Bersabe and caballero et al. (2016)
-  lambda <- P*N #mean number of mutations in population
+  lambda <- mut*N #mean number of mutations in population
   N.mutations    <- rpois(1,lambda)
   rows <- sample(1:N, N.mutations)
-  sapply(rows, MUTATION)   # Apply function MUTATION to generate mutant loci
+  Individuals[rows,"BirthMut",1] <- Individuals[rows,"BirthMut",1] + sample(c(-1,1), 1) *rgamma(1, shape = 0.5, rate = 10)
+  rows <- sample(1:N, N.mutations)
+  Individuals[rows,"GrowMut",1] <- Individuals[rows,"GrowMut",1] + sample(c(-1,1), 1) *rgamma(1, shape = 0.5, rate = 10)
   
   #copy to next array before adding mutation
   #num_alive = nrow(Individuals[Individuals[,"Alive",1] == TRUE,,1])
@@ -125,14 +117,15 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
       
       # Phenotypic values
       #two column matrix of parents average genetic values for a and b, 1/2 both additive variance
-      Individuals[,"Phenotype",Iyear] <- Individuals[,"GrowPhen",Iyear] <- (Individuals[,"BirthPhen",Iyear] + Individuals[,"BirthGen",Iyear] + Individuals[,"GrowGen",Iyear] + Individuals[,"BirthEnv",Iyear] + Individuals[,"GrowEnv",Iyear])*Individuals[,"Age",Iyear]
+      Individuals[,"BirthPhen",Iyear] <- Individuals[,"BirthGen",Iyear] + Individuals[,"BirthEnv",Iyear]
+      Individuals[,"GrowPhen",Iyear] <- Individuals[,"GrowPhen",Iyear] <- (Individuals[,"BirthPhen",Iyear] + Individuals[,"BirthGen",Iyear] + Individuals[,"GrowGen",Iyear] + Individuals[,"BirthEnv",Iyear] + Individuals[,"GrowEnv",Iyear])*Individuals[,"Age",Iyear]
       
       # Winter Survival
       Individuals[,"Alive",Iyear] <- WINTER_SURVIVAL(Individuals[,"Phenotype",Iyear]) 
       
       # Fitness calculation
-      Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Phenotype",Iyear])
-      Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Phenotype",Iyear])
+      Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"GrowPhen",Iyear])
+      Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"GrowPhen",Iyear])
     } # End of else if
     else {
       #copy over alive from past generation
@@ -145,26 +138,28 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
       #Genotypic value
       print(Iyear)
       
-      Iyear_moms = Individuals[which(Individuals[,"Sex",Iyear-1] == TRUE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowGen",Iyear-1] + Individuals[which(Individuals[,"Sex",Iyear-1] == TRUE & Individuals[,"Alive",Iyear-1] == TRUE),"Mutation",Iyear-1]
-      Iyear_dads = Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowGen",Iyear-1] + Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"Mutation",Iyear-1]
+      Iyear_moms = Individuals[which(Individuals[,"Sex",Iyear-1] == TRUE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowGen",Iyear-1] + Individuals[which(Individuals[,"Sex",Iyear-1] == TRUE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowMut",Iyear-1]
+      Iyear_dads = Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowGen",Iyear-1] + Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"GrowMut",Iyear-1]
       # offspring value is average of parents plus random deviation; average of parents+0.5*mutation+random deviation
       #pick parents based on fitness, and grab row index from that year (use sample.int function to sample individuals row in previous year) for parents, mother index vector, father index vector
       Individuals[(num_alive+1):N,"BirthGen",Iyear] <- (sample(Iyear_moms, N-num_alive, replace = TRUE, Individuals[which(Individuals[,"Sex",Iyear-1] == TRUE & Individuals[,"Alive",Iyear-1] == TRUE),"Fitness",Iyear-1]+ROUNDING) 
-                                                       + sample(Iyear_dads, N-num_alive, replace = TRUE, Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"Fitness",Iyear-1]+ROUNDING))/2 + rnorm(N-num_alive,0,sqrt(Va/2))
+                                                       + sample(Iyear_dads, N-num_alive, replace = TRUE, Individuals[which(Individuals[,"Sex",Iyear-1] == FALSE & Individuals[,"Alive",Iyear-1] == TRUE),"Fitness",Iyear-1]+ROUNDING))/2 + rnorm(N-num_alive,0,sqrt(mut/2))
      
       # Mutation values
       # Number of mutations using a Poisson distribution
-      lambda         <- 2*P*(N-num_alive) #mean number of mutations in population
+      # P is mutation rate
+      lambda         <- 2*mut*(N-num_alive) #mean number of mutations in population
       N.mutations    <- rpois(1,lambda)
       rows <- sample(N, N.mutations)
-      sapply(rows, MUTATION, Iyear = Iyear) # Apply function MUTATION to generate mutant loci
+      Individuals[rows,"BirthMut",Iyear] <- Individuals[rows,"BirthMut",Iyear] + sample(c(-1,1), 1) *rgamma(1, shape = 0.5, rate = 10)
+      rows <- sample(1:N, N.mutations)
+      Individuals[rows,"GrowMut",Iyear] <- Individuals[rows,"GrowMut",Iyear] + sample(c(-1,1), 1) *rgamma(1, shape = 0.5, rate = 10)
       
       # Sex of Offspring
       Individuals[(num_alive+1):N,"Sex",Iyear] <- sample(c(0,1), N-num_alive, replace = T) #set all sexes
-      
+
       # Environmental values
-      Individuals[,"BirthEnv",Iyear] 	  	<- Individuals[,"BirthGen",Iyear] + grfx(N, E, warn = F)   # Environmental values
-      Individuals[,"GrowEnv",Iyear] 	  	<- Individuals[,"GrowGen",Iyear] + grfx(N, E, warn = F)   # Environmental values 
+      Individuals[,c("BirthEnv", "GrowEnv"),Iyear] 	  	<- Individuals[,"BirthGen",Iyear] + grfx(N, E, warn = F)   # Environmental values
       
       # Phenotypic values
       Individuals[,"GrowPhen",Iyear] <- (Individuals[,"BirthPhen",Iyear] + Individuals[,"BirthGen",Iyear] + Individuals[,"GrowGen",Iyear] + Individuals[,"BirthEnv",Iyear] + Individuals[,"GrowEnv",Iyear])*Individuals[,"Age",Iyear]
@@ -175,12 +170,12 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
       #Individuals[,"Phenotype",Iyear] <- Individuals[,"Phenotype",Iyear] + Individuals[,"Growth", Iyear]
       
       # Winter Survival
-      Individuals[,"Alive",Iyear] <- WINTER_SURVIVAL(Individuals[,"Phenotype",Iyear]) 
+      Individuals[,"Alive",Iyear] <- WINTER_SURVIVAL(Individuals[,"GrowPhen",Iyear]) 
       
       #browser()
       # Fitness calculation
-      Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Phenotype",Iyear])
-      Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Phenotype",Iyear])
+      Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == FALSE & Individuals[,"Alive",Iyear] == TRUE),"GrowPhen",Iyear])
+      Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"Fitness",Iyear] <- REL_FITNESS(Individuals[which(Individuals[,"Sex",Iyear] == TRUE & Individuals[,"Alive",Iyear] == TRUE),"GrowPhen",Iyear])
     } # End of else
     
     #add one to ages
@@ -194,6 +189,10 @@ SIMULATION <- function(N = 100, MaxYear = 10, h2 = matrix(c(0.5,0.0, 0, .3), nro
   #plot(apply(Individuals[,"Phenotype",,drop = TRUE], MARGIN = 1, FUN = var))
   #plot(apply(Individuals[,"Genetic",,drop = TRUE], MARGIN = 1, FUN = var))
   #return(mean(Individuals[,"Phenotype",Iyear]))
+  #*****plot birth and growth of individuals at different intervals*******
+  #*****histogram of ages******
+  #*****plot phenotypes*******
+  #scale by mean to compare generations ((sqrt(variance)/mean)*100)
   print(Individuals[,,Iyear])
 }
 
